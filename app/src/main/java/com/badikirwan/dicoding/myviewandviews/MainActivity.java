c9d2a552d7c36080eb88302b0e8325cbf52195d2
badikirwan <badikirwan@gmail.com>
2018-07-17 02:51:36 +0700

    Initial commit

---
+++ app/src/main/java/com/badikirwan/dicoding/myviewandviews/MainActivity.java
@@ -0,0 +1,14 @@
+package com.badikirwan.dicoding.myviewandviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Google Pixel");
+    }
+}
